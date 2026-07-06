f8428d5d7dfb3fe1989147d6a329b5877a81604a
Akshat <146753668+gaur-Akshat@users.noreply.github.com>
2026-07-07 04:32:38 +0530

    home page 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import "./index.css";
 // Pages
 import Home from "./pages/Home";
 import ResultsAWC2526 from "./pages/ResultsAWC2526";
-import ResultsAWC2425 from "./pages/ResultsAWC2425";
 import TeamsAWC2526 from "./pages/TeamsAWC2526";
-import TeamsAWC2425 from "./pages/TeamsAWC2425";
 import Committee from "./pages/Committee";
 import OrganisingCommittee from "./pages/OrganisingCommittee";
 import About from "./pages/About";
